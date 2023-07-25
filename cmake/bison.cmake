3d838f0967f3bb2d13cd900a3cb8798d1c9c83ba
ruanshudong <ruanshudong@qq.com>
2023-07-25 17:51:23 +0800

    fix .github

--- cmake/bison.cmake
+++ cmake/bison.cmake
@@ -10,7 +10,7 @@ ExternalProject_Add(ADD_BISON
         BUILD_IN_SOURCE 1
         LOG_BUILD 1
         LOG_CONFIGURE 0
-        BUILD_COMMAND make -j4
+        BUILD_COMMAND make
         INSTALL_COMMAND make install
         URL_MD5 ${BISON_MD5}
         )
